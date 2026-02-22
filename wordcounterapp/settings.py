11f1e80679e907668f5af5303f211d6c4d8e429f
Roy Peters <reory35@hotmail.com>
2026-02-22 01:08:06 +0000

    Security: Move secret key and DEBUG to environment variable.

--- wordcounterapp/settings.py
+++ wordcounterapp/settings.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
 """
 Django settings for wordcounterapp project.
 
@@ -10,20 +13,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ozk)k#9y3azpio)!gncd30#shwg(52u*_^^-*6xushn8vho9!r"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
